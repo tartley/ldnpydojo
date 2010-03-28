0032d0af693bae04daac3a5df5e188d0d6d60b65
tartley <tartley@4748adc8-e312-874e-4c17-05773901bfc9>
2010-03-28 16:46:53 +0000

    fixed creation of Body at right position

--- items.py
+++ items.py
@@ -1,50 +1,28 @@
 
-from pymunk import Body, Poly, moment_for_poly
+from pymunk import Body, Poly, moment_for_poly, Vec2d
 
 
 class Platform(object):
 
-    def __init__(self, x, y, angle=0.0):
-        self.width = 400
-        self.height = 50
+    def __init__(self, x, y, width=400, height=50):
+        self.width = width
+        self.height = height
 
-        verts = (
-            (x - self.width / 2, y - self.height / 2),
-            (x - self.width / 2, y + self.height / 2),
-            (x + self.width / 2, y + self.height / 2),
-            (x + self.width / 2, y - self.height / 2),
-        )
+        verts = [
+            (- self.width / 2, - self.height / 2),
+            (- self.width / 2, + self.height / 2),
+            (+ self.width / 2, + self.height / 2),
+            (+ self.width / 2, - self.height / 2),
+        ]
+        verts = map(Vec2d, verts)
         self.mass = self.width * self.height
         self.body = Body(
             self.mass, moment_for_poly(self.mass, verts))
+        self.body.position = (x, y)
         self.shape = Poly(self.body, verts, (0, 0))
-
-
-    @property
-    def x(self):
-        return self.body.position.x
-
-
-    @property
-    def y(self):
-        return self.body.position.y
-
-
-    @property
-    def angle(self):
-        return self.body.angle
 
 
     @property
     def verts(self):
         return self.shape.get_points()
-
-
-
-class Player(object):
-
-    def __init__(self, x, y, angle=0.0):
-        self.x = x
-        self.y = y
-        self.angle = angle
 
--- world.py
+++ world.py
@@ -1,12 +1,16 @@
 
-from pymunk import Space
+from pymunk import init_pymunk, Space
 
 
 class World(object):
 
     def __init__(self):
         self.items = []
+        init_pymunk()
         self.space = Space()
+        self.space.resize_static_hash()
+        self.space.resize_active_hash()
+
 
     def add(self, item):
         self.items.append(item)
@@ -14,5 +18,5 @@ class World(object):
         self.space.add(item.body)
 
     def update(self):
-        self.space.step(1)
+        self.space.step(1.0)
 
