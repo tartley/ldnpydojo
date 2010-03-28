9c4034f4bf28b70fd8c6c419f4c5715c26c85332
tomviner <tomviner@4748adc8-e312-874e-4c17-05773901bfc9>
2010-03-28 19:47:52 +0000

    added draw_spring method

--- trunk/render.py
+++ trunk/render.py
@@ -13,6 +13,11 @@ class Render(object):
         self.window.display_surface.fill((0, 0, 0))
         for item in self.world.items:
             self.draw_platform(item)
+        self.draw_spring()
+
+    def draw_spring(self):
+        p1, p2 = self.world.items
+        draw.line(self.window.display_surface, (80, 120, 90), p1.centre, p2.centre, 15)
 
 
     def draw_platform(self, item):
