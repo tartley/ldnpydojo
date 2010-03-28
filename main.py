08fc32a66b069ac46c29ae19d8830d7e91c34965
tartley <tartley@4748adc8-e312-874e-4c17-05773901bfc9>
2010-03-28 14:47:17 +0000

    split classes into own modules

--- main.py
+++ main.py
@@ -1,87 +1,15 @@
 
 from __future__ import division
 
-from pygame import display, draw, event
-from pygame.locals import *
+from pygame import display, event
+from pygame.locals import QUIT, K_ESCAPE
 
-from pymunk import Space, Body, Poly, moment_for_poly
+from pymunk import DampedSpring
 
-
-class Window(object):
-
-    def init(self):
-        display.init()
-        modes = display.list_modes()
-        self.display_surface = display.set_mode(modes[0],
-            HWSURFACE | DOUBLEBUF | FULLSCREEN)
-
-    @property
-    def width(self):
-        return self.display_surface.get_width()
-
-
-class World(object):
-
-    def __init__(self):
-        self.items = []
-        self.space = Space()
-
-    def add(self, item):
-        self.items.append(item)
-        self.space.add(item.shape)
-        self.space.add(item.body)
-
-    def update(self):
-        self.space.step(1)
-
-
-
-class Platform(object):
-
-    def __init__(self, x, y, angle=0.0):
-        self.x = x
-        self.y = y
-        self.angle = angle
-        self.width = 400
-        self.height = 50
-
-        verts = (
-            (self.x - self.width / 2, self.y - self.height / 2),
-            (self.x - self.width / 2, self.y + self.height / 2),
-            (self.x + self.width / 2, self.y + self.height / 2),
-            (self.x + self.width / 2, self.y - self.height / 2),
-        )
-        self.mass = self.width * self.height
-        self.body = Body(
-            self.mass, moment_for_poly(self.mass, verts))
-        self.shape = Poly(self.body, verts, (0, 0))
-        self.body.apply_impulse((0, 100000), (500, 50))
-
-
-    @property
-    def verts(self):
-        return self.shape.get_points()
-
-
-
-class Render(object):
-
-    def __init__(self, window, world):
-        self.window = window
-        self.world = world
-
-
-    def draw_world(self):
-        self.window.display_surface.fill((0, 0, 0))
-        for item in self.world.items:
-            self.draw_platform(item)
-
-
-    def draw_platform(self, item):
-        draw.polygon(
-            self.window.display_surface,
-            (255, 0, 0),
-            item.verts, 0)
+from window import Window
+from world import World
+from items import Platform
+from render import Render
 
 
 def main():
@@ -89,7 +17,15 @@ def main():
     window.init()
 
     world = World()
-    world.add(Platform(window.width / 2, 100))
+
+    p1 = Platform(window.width / 2, 100)
+    p2 = Platform(window.width / 2, 500)
+    world.add(p1)
+    world.add(p2)
+
+    spring = DampedSpring(p1.body, p2.body, (0, 100), (100, 100), 40, 100, 10)
+    world.space.add(spring)
+    p1.body.apply_impulse((0, 0), (500000, 50000))
 
     render = Render(window, world)
 
