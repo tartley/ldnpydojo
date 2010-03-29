e49a5f02e1fa1fde97c68490e3e0a97e1f1b7453
tomviner <tomviner@4748adc8-e312-874e-4c17-05773901bfc9>
2010-03-29 19:48:04 +0000

    refactored to create spring class

--- items.py
+++ items.py
@@ -1,6 +1,17 @@
 from __future__ import division
 
-from pymunk import Body, Poly, moment_for_poly, Vec2d
+from pymunk import Body, DampedSpring, Poly, moment_for_poly, Vec2d
+
+
+
+class Spring(DampedSpring):
+
+    def __init__(self, p1, p2, offset1, offset2, verts_to_anchor1, verts_to_anchor2, rest_length, stiffness, damping):    
+        self.p1 = p1
+        self.p2 = p2
+        self.verts_to_anchor1 = verts_to_anchor1
+        self.verts_to_anchor2 = verts_to_anchor2
+        super(Spring, self).__init__(p1.body, p2.body, offset1, offset2, rest_length, stiffness, damping)
 
 
 class Platform(object):
@@ -25,12 +36,53 @@ class Platform(object):
 
     @property
     def verts(self):
-        return self.shape.get_points()
+        return self.shape.get_points()        
 
     @property
     def centre(self):
         verts = self.verts
-        average = lambda ns:sum(ns)/len(ns)
-        return average((verts[0].x, verts[2].x)), average((verts[0].y, verts[2].y))
+        return self.mid_point(verts[0], verts[2])
 
     
+    @staticmethod
+    def point_offset_to_abs(vec, centre):
+        return vec[0]+centre[0], vec[1]+centre[1]
+    @staticmethod
+    def verts_offset_to_abs(verts, centre):
+        return [self.point_offset_to_abs(vec, self.centre) for vec in verts]
+
+
+    @staticmethod
+    def point_abs_to_offset(point, centre):
+        return point[0]-centre[0], point[1]-centre[1]
+    @staticmethod
+    def verts_abs_to_offset(points, centre):
+        return [self.point_abs_to_offset(point, self.centre) for point in points]    
+
+
+
+    @staticmethod
+    def mid_point(vec1, vec2):
+        average = lambda ns:sum(ns)/len(ns)
+        return average((vec1.x, vec2.x)), average((vec1.y, vec2.y))
+
+        
+    @property
+    def abs_mid_left_end(self):
+        verts = self.verts
+        return self.mid_point(verts[0], verts[1])
+    
+    @property
+    def abs_mid_right_end(self):
+        verts = self.verts
+        return self.mid_point(verts[2], verts[3])
+
+
+    @property
+    def offset_mid_left_end(self):
+        return self.point_abs_to_offset(self.abs_mid_left_end, self.centre)
+    
+    @property
+    def offset_mid_right_end(self):
+        return self.point_abs_to_offset(self.abs_mid_right_end, self.centre)
+        
--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from __future__ import division
 
 from pygame import display, event
 from pygame.locals import QUIT, K_ESCAPE
-
-from pymunk import DampedSpring
+from pymunk import Vec2d
 
 from window import Window
 from world import World
-from items import Platform
+from items import Platform, Spring
 from render import Render
 
 
+
+        
 def main():
     window = Window()
     window.init()
@@ -21,15 +22,42 @@ def main():
     world.add(p1)
     p2 = Platform(300, 500, 800, 100)
     world.add(p2)
-    spring = DampedSpring(p1.body, p2.body, (0, 0), (0, 0), 200, 10, 1)
-    world.space.add(spring)
 
-    sensible = 0
-    if sensible:
-        p1.body.apply_impulse((15000, 0), (-100, 0))
-    else:
-        p1.body.apply_impulse((+280000, 10000), (-100, 0))
-        p2.body.apply_impulse((-280000, -000), (-100, 0))
+# vert order:
+#    0 3
+#    1 2
+    spring = Spring(p1, p2,
+                    offset1=Vec2d(p1.verts[2])-Vec2d(p1.centre),
+                    offset2=Vec2d(p2.verts[3])-Vec2d(p2.centre),
+                    verts_to_anchor1=lambda vs:vs[2],
+                    verts_to_anchor2=lambda vs:vs[3],
+                    rest_length=500, stiffness=100, damping=100)
+    world.add_spring(spring)
+
+    spring = Spring(p1, p2,
+                    offset1=Vec2d(p1.verts[2])-Vec2d(p1.centre),
+                    offset2=Vec2d(p2.verts[3])-Vec2d(p2.centre),
+                    verts_to_anchor1=lambda vs:vs[2],
+                    verts_to_anchor2=lambda vs:vs[3],
+                    rest_length=500, stiffness=100, damping=100)
+    world.add_spring(spring)
+
+    spring = Spring(p1, p2,
+                    offset1=(0, 0),
+                    offset2=(0, 0),
+                    verts_to_anchor1=lambda vs: (vs[1] + vs[3])/2,
+                    verts_to_anchor2=lambda vs: (vs[1] + vs[3])/2,
+                    rest_length=500, stiffness=1000, damping=100)
+    world.add_spring(spring)   
+    
+
+    
+##    sensible = 1
+##    if sensible:
+##        p1.body.apply_impulse((0, +5000), (-100, 0))
+##    else:
+##        p1.body.apply_impulse((+280000, 10000), (-100, 0))
+##        p2.body.apply_impulse((-280000, -000), (-100, 0))
 
     render = Render(window, world)
 
