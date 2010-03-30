15cf3115a83ed5688bdc827a06942884880f2d42
tomviner <tomviner@4748adc8-e312-874e-4c17-05773901bfc9>
2010-03-30 21:32:36 +0000

    spring connected to 1 fixed & 1 mobile platform

--- trunk/main.py
+++ trunk/main.py
@@ -1,7 +1,8 @@
 from __future__ import division
 
-from pygame import display, event
+from pygame import display, event, Rect
 from pygame.locals import QUIT, KEYDOWN, K_ESCAPE, K_RETURN, KMOD_ALT
+from pymunk import Vec2d
 
 from window import Window
 from world import World
@@ -10,33 +11,37 @@ from render import Render
 
 
 
-def populate(world):
+def populate(window, world):
+    base_size = Vec2d(2000, 100)
+    base_centre = (base_size[0]/2, window.height-60)
+    
+    base_topleft = Vec2d(base_centre - base_size/2)
+    base = Platform(*Rect(base_centre, base_size), static=True)
+    world.add_item(base)
 
-    p1 = Platform(600, 300, 400, 50)
-    world.add_item(p1)
-    p2 = Platform(500, 600, 800, 100, static=True)
-    world.add_item(p2)
+    platform = Platform(600, window.height-300, 400, 50)
+    world.add_item(platform)    
     
     """   vert order:
              0 3
              1 2
     """
-    spring = Spring(p1, p2,
+    spring = Spring(base, platform,
+                    lambda _:base_topleft,
                     lambda vs:vs[1],
-                    lambda vs:vs[0],
                     200, 10, 1)
     world.add_spring(spring)
 
-    spring = Spring(p1, p2,
+    spring = Spring(base, platform,
+                    lambda _:base_topleft+(1600, 0),
                     lambda vs:vs[2],
-                    lambda vs:vs[3],
                     200, 10, 1)
     world.add_spring(spring)
 
-    spring = Spring(p1, p2,
+    spring = Spring(base, platform,
+                    lambda v_:base_topleft+(800, 0),
                     lambda vs: (vs[1] + vs[3])/2,
-                    lambda vs: (vs[1] + vs[3])/2,
-                    200, 100, 1)
+                    400, 100, 1)
     world.add_spring(spring)
 
     world.add_item(Woger(300, 000))
@@ -47,7 +52,7 @@ def main():
     window.init()
 
     world = World()
-    populate(world)
+    populate(window, world)
 
     render = Render(window, world)
 
