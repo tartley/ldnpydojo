4350da870f69df8d87172e7caf1c410d074775f5
tartley <tartley@4748adc8-e312-874e-4c17-05773901bfc9>
2010-03-30 21:10:06 +0000

    alt-return toggles fullscreen

--- window.py
+++ window.py
@@ -3,15 +3,30 @@ from pygame import display
 from pygame.locals import HWSURFACE, DOUBLEBUF, FULLSCREEN
 
 
+FLAGS = HWSURFACE | DOUBLEBUF
+
 
 class Window(object):
 
     def init(self):
+        self.fullscreen = False
         display.init()
+        self.set_mode()
+
+
+    def set_mode(self):
         modes = display.list_modes()
-        self.display_surface = display.set_mode(modes[0],
-            HWSURFACE | DOUBLEBUF )
-            #HWSURFACE | DOUBLEBUF)
+        self.display_surface = display.set_mode(modes[0], self.get_flags())
+
+
+    def get_flags(self):
+        return FLAGS if not self.fullscreen else FLAGS | FULLSCREEN
+
+
+    def toggle_fullscreen(self):
+        self.fullscreen = not self.fullscreen
+        self.set_mode()
+
 
     @property
     def width(self):
