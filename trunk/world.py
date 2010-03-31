612f72fbbcff3e194681749d2c37f92f4ad1a24f
tartley <tartley@4748adc8-e312-874e-4c17-05773901bfc9>
2010-03-31 17:59:09 +0000

    move world population into world.py

--- trunk/world.py
+++ trunk/world.py
@@ -1,12 +1,45 @@
 
+from math import pi
+from random import randint, uniform
+
 from pymunk import init_pymunk, Space
+
+from items import Ground, Branch, Bough, Woger
+
+
+def populate(world):
+    ground = Ground()
+    world.add_item(ground)
+
+    def add_branch(parent, angle, thickness, length):
+        branch = Branch(parent, angle, thickness, length)
+        world.add_item(branch)
+
+        bough = Bough(branch)
+        world.add_item(bough)
+
+        if thickness > 25:
+            branches = randint(3, 4)
+            spread = uniform(pi / 8, pi / branches)
+            for i in xrange(branches):
+                delta_angle = spread * (branches - 1) / 2 - spread * i
+                newangle = angle + delta_angle
+                newlength = length * (1 - abs(delta_angle) / 2.5)
+                newthickness = thickness * 0.75
+                add_branch( branch, newangle, newthickness, newlength )
+        return branch
+
+    trunk = add_branch(ground, 0, 50, 400)
+
+    woger = Woger(200, 1000)
+    world.add_item(woger)
+
 
 
 class World(object):
 
     def __init__(self):
         self.items = []
-        self.springs = []
         init_pymunk()
         self.space = Space()
         self.space.gravity = (0, -0.1)
@@ -17,10 +50,6 @@ class World(object):
         self.items.append(item)
         item.create_body()
         item.add_to_space(self.space)
-
-    def add_spring(self, spring):
-        self.springs.append(spring)
-        self.space.add(spring)
 
     def update(self):
         self.space.step(1)
