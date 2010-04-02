bf5af7a169a5c17fb04e38dbf9641131faf1a18b
renesd <renesd@4748adc8-e312-874e-4c17-05773901bfc9>
2010-04-02 12:06:01 +0000

    set window to 800x600

--- window.py
+++ window.py
@@ -10,15 +10,20 @@ FLAGS = HWSURFACE | DOUBLEBUF
 class Window(object):
 
     def init(self):
-        environ['SDL_VIDEO_WINDOW_POS'] = '0,0'
+        # buggy on some OSX boxen.
+        #environ['SDL_VIDEO_WINDOW_POS'] = '0,0'
+
+        environ['SDL_VIDEO_CENTERED'] = '1'
         self.fullscreen = False
         display.init()
         self.set_mode()
 
 
     def set_mode(self):
-        modes = display.list_modes()
-        self.display_surface = display.set_mode(modes[0], self.get_flags())
+        #modes = display.list_modes()
+        #self.display_surface = display.set_mode(modes[0], self.get_flags())
+        self.display_surface = display.set_mode((800,600), self.get_flags())
+
         self.width = self.display_surface.get_width()
         self.height = self.display_surface.get_height()
         display.set_caption("Woger the Wibbly Wobbly Wombat")
