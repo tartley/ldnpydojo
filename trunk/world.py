f8ffe1124b750d8c0f2d1f7b40ba0f69707ea55a
renesd <renesd@4748adc8-e312-874e-4c17-05773901bfc9>
2010-04-02 13:44:50 +0000

    powerup on getting orange

--- trunk/world.py
+++ trunk/world.py
@@ -64,9 +64,11 @@ def populate(world):
         owanges = [s.parent for s in arbiter.shapes 
                       if hasattr(s, 'parent') and isinstance(s.parent, Owange)]
 
-                
+        
         for o in owanges:
+            Sounds.sounds.play("powerup1")
             world.remove_item(o)
+
         return 1
  
     def leave_owange(space, arbiter, woger):
