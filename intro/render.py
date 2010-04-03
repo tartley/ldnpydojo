1ec32177557b2a53f80c798b000cef33bcd8b97e
tomviner <tomviner@4748adc8-e312-874e-4c17-05773901bfc9>
2010-04-03 19:54:11 +0000

    started on intro

---
+++ intro/render.py
@@ -0,0 +1,38 @@
+
+from pygame import draw
+
+from .items import Platform
+
+
+class Render(object):
+
+    def __init__(self, window, world):
+        self.window = window
+        self.world = world
+
+
+    def draw_world(self):
+        self.window.display_surface.fill((0, 0, 0))
+        for item in self.world.items:
+            self.draw_platform(item)
+        for spring in self.world.springs:
+            self.draw_spring(spring)
+        
+
+    def draw_spring(self, spring):
+        draw.line(
+            self.window.display_surface,
+            (80, 120, 90),
+            spring.verts_to_anchor1(spring.p1.verts),
+            spring.verts_to_anchor2(spring.p2.verts),
+            15)
+
+    def draw_platform(self, item):
+        draw.polygon(
+            self.window.display_surface,
+            (255, 0, 0),
+            item.verts, 0)
+
+    def draw_word(self, word):
+        pass
+
---
+++ intro/world.py
@@ -0,0 +1,26 @@
+
+from pymunk import init_pymunk, Space
+
+
+class World(object):
+
+    def __init__(self):
+        self.items = []
+        self.springs = []
+        init_pymunk()
+        self.space = Space()
+        self.space.resize_static_hash()
+        self.space.resize_active_hash()
+
+    def add_item(self, item):
+        self.items.append(item)
+        self.space.add(item.shape)
+        self.space.add(item.body)
+
+    def add_spring(self, spring):
+        self.springs.append(spring)
+        self.space.add(spring)
+
+    def update(self):
+        self.space.step(1.0)
+
