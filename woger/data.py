655baba368210fe5a627ffd82c95ce6733e87683
renesd <renesd@4748adc8-e312-874e-4c17-05773901bfc9>
2010-04-04 12:49:32 +0000

    try to find data path if it is installed as a package

--- woger/data.py
+++ woger/data.py
@@ -4,7 +4,10 @@ _data_dir = None
 def data_dir():
     global _data_dir
     if _data_dir is None:
-        paths = (['woger', 'data'], ['data'])
+        paths = (['woger', 'data'], 
+                 ['data'],
+                 [os.path.split(__file__)[0], 'data'],
+                )
         for p in paths:
             apath = os.path.join(*p)
             if os.path.exists(apath):
